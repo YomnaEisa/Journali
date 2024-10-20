104d3a8ab09ec9b353b80326fc16f37c3d56d78e
Yomna <yomna.ar.eisa@gmail.com>
2024-10-20 10:51:47 +0300

    Initial Commit

---
+++ Journali/JournaliApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournaliApp.swift
+//  Journali
+//
+//  Created by Yomna Eisa on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct JournaliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JournaliTests/JournaliTests.swift
@@ -0,0 +1,17 @@
+//
+//  JournaliTests.swift
+//  JournaliTests
+//
+//  Created by Yomna Eisa on 20/10/2024.
+//
+
+import Testing
+@testable import Journali
+
+struct JournaliTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
